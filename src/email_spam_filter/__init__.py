1f3f41665fa98d5525238303a580d587d07dc0e7
Jack Swerdlow <js2596@cam.ac.uk>
2025-06-03 23:13:42 +0100

    Add common module with constants, utility functions and paths

--- src/email_spam_filter/__init__.py
+++ src/email_spam_filter/__init__.py
@@ -1,1 +1,13 @@
-"""Repository for the email-spam-filter project."""
+"""Repository for the email-spam-filter project.
+
+Modules:
+    common: Common utilities and shared data structures for the email-spam-filter project.
+"""
+
+from __future__ import annotations
+
+__all__ = ("common",)
+
+from email_spam_filter import (
+    common,
+)
