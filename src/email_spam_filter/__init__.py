3f71d922cd9a172f1adf1d227493208701e9c8ba
Jack Swerdlow <js2596@cam.ac.uk>
2025-06-03 23:18:52 +0100

    Add data module and data collection submodule

--- src/email_spam_filter/__init__.py
+++ src/email_spam_filter/__init__.py
@@ -2,12 +2,17 @@
 
 Modules:
     common: Common utilities and shared data structures for the email-spam-filter project.
+    data: Data ingestion, pre-processing, and labelling utilities for the email-spam-filter project.
 """
 
 from __future__ import annotations
 
-__all__ = ("common",)
+__all__ = (
+    "common",
+    "data",
+)
 
 from email_spam_filter import (
     common,
+    data,
 )
