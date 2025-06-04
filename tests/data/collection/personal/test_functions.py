2a9b50210eb8ce812026a1b06a68e596dfc724ea
Jack Swerdlow <js2596@cam.ac.uk>
2025-06-04 19:26:03 +0100

    Refactor tests to use pytest_mock instead of unittest.mock

--- tests/data/collection/personal/test_functions.py
+++ tests/data/collection/personal/test_functions.py
@@ -2,38 +2,40 @@
 
 from __future__ import annotations
 
-import unittest.mock
+import typing
 
 import keyring
 import pytest
 
 from email_spam_filter.data.collection.personal.functions import get_imap_password
 
+if typing.TYPE_CHECKING:
+    import pytest_mock
+
 
 class TestGetIMAPPassword:
     @staticmethod
-    @unittest.mock.patch(
-        "email_spam_filter.data.collection.personal.functions.keyring.get_password"
-    )
-    def test_success(mock_get_password: unittest.mock.MagicMock) -> None:
-        mock_get_password.return_value = "secure-password"
+    def test_success(mocker: pytest_mock.MockerFixture) -> None:
+        mocker.patch(
+            "email_spam_filter.data.collection.personal.functions.keyring.get_password",
+            return_value="secure-password",
+        )
         assert get_imap_password() == "secure-password"
 
     @staticmethod
-    @unittest.mock.patch(
-        "email_spam_filter.data.collection.personal.functions.keyring.get_password"
-    )
-    def test_missing_password_raises(mock_get_password: unittest.mock.MagicMock) -> None:
-        mock_get_password.return_value = None
+    def test_missing_password_raises(mocker: pytest_mock.MockerFixture) -> None:
+        mocker.patch(
+            "email_spam_filter.data.collection.personal.functions.keyring.get_password",
+            return_value=None,
+        )
         with pytest.raises(RuntimeError, match="IMAP password not found in keyring"):
             get_imap_password()
 
     @staticmethod
-    @unittest.mock.patch(
-        "email_spam_filter.data.collection.personal.functions.keyring.get_password",
-        side_effect=keyring.errors.NoKeyringError("No backend found"),
-    )
-    def test_keyring_error(mock_get_password: unittest.mock.MagicMock) -> None:
-        mock_get_password.return_value = None
+    def test_keyring_error(mocker: pytest_mock.MockerFixture) -> None:
+        mocker.patch(
+            "email_spam_filter.data.collection.personal.functions.keyring.get_password",
+            side_effect=keyring.errors.NoKeyringError("No backend found"),
+        )
         with pytest.raises(RuntimeError, match="No functional keyring backend"):
             get_imap_password()
