055feb93c47368d13a4dcef7a5a257e4fd1a1998
Jack Swerdlow <js2596@cam.ac.uk>
2025-06-09 00:29:34 +0100

    Refactor constants to work with UserConfig

--- src/email_spam_filter/common/constants.py
+++ src/email_spam_filter/common/constants.py
@@ -4,16 +4,16 @@ from __future__ import annotations
 
 from email_spam_filter.common.functions import load_user_config
 
-_config = load_user_config()
+CONFIG = load_user_config()
 
-USER_EMAIL: str = _config["user_email"]
+USER_EMAIL: str = CONFIG.user_email
 """Email address used to log in to the IMAP server."""
 
-IMAP_HOST: str = _config["imap_host"]
+IMAP_HOST: str = CONFIG.imap_host
 """IMAP server host for the email provider."""
 
-KEYRING_SERVICE: str = _config["keyring_service"]
+KEYRING_SERVICE: str = CONFIG.keyring_service
 """Service name used by keyring for password retrieval."""
 
-FOLDER_MAP: dict[str, str] = _config["folder_map"]
+FOLDER_MAP: dict[str, str] = CONFIG.folder_map
 """Mapping of IMAP folder names to short local labels."""
