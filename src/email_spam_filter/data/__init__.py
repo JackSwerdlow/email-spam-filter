8999311664a7e53b53fc6891cf48c6ee642f3bd6
Jack Swerdlow <js2596@cam.ac.uk>
2025-06-18 17:35:30 +0100

    Update data modules  __init__.py

--- src/email_spam_filter/data/__init__.py
+++ src/email_spam_filter/data/__init__.py
@@ -3,6 +3,7 @@
 Modules:
     collection: Source-specific data collection utilities for acquiring external datasets.
     io: Input/output utilities for reading, writing, and processing email data.
+    labelling: Manual labelling utilities for supervised learning workflows.
     organise: Source-specific pre-processing utilities for external datasets.
 """
 
@@ -11,11 +12,13 @@ from __future__ import annotations
 __all__ = (
     "collection",
     "io",
+    "labelling",
     "organise",
 )
 
 from email_spam_filter.data import (
     collection,
     io,
+    labelling,
     organise,
 )
