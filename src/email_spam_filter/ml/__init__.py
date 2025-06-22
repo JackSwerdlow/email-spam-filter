2fd540d9ba05bcd22b6aa3df0e5bda90bfd0b102
Jack Swerdlow <js2596@cam.ac.uk>
2025-06-22 21:13:35 +0100

    Add ml.logistic_regression submodule

--- src/email_spam_filter/ml/__init__.py
+++ src/email_spam_filter/ml/__init__.py
@@ -2,6 +2,7 @@
 
 Modules:
     common: Common utilities and shared data structures for model development and evaluation.
+    logistic_regression: Binary linear classifier trained via maximum likelihood estimation.
 """
 
 from __future__ import annotations
